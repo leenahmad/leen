c90eead37fbcbdb8dcf26544e542ca043d0473c6
leenahmad <leenkaraja24@gmail.com>
2022-01-01 23:17:55 +0200

    almost done

---
+++ draw.js
@@ -0,0 +1,156 @@
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth - 60;
+canvas.height = 400;
+
+let context = canvas.getContext("2d");
+let start_background_color = "white";
+context.fillStyle = "white";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+let draw_color = "black";
+let draw_width = "2";
+let is_drawing = false;
+let restore_array = [];
+let index = -1;
+
+function change_color(element){
+    draw_color = element.style.background;
+}
+
+
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+canvas.addEventListener("touchend", stop, false);
+canvas.addEventListener("mouseup", stop, false);
+canvas.addEventListener("mouseout", stop, false);
+
+
+function start(event) {
+    is_drawing = true;
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+
+    event.preventDefault();
+}
+
+function draw(event){
+    if (is_drawing) {
+        context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        context.strokeStyle = draw_color;
+        context.lineWidth = draw_width;
+        context.lineCap = "round";
+        context.lineJoin = "round";
+        context.stroke();
+
+    }
+    event.preventDefault();
+}
+
+function stop(event){
+    if (is_drawing){
+        context.stroke();
+        context.closePath();
+        is_drawing = false;
+    }
+    event.preventDefault();
+
+if ( event.type != 'mouseout' ){
+    restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    index += 1;
+}
+    
+}
+
+// Set up touch events for mobile, etc
+
+canvas.addEventListener("touchstart", function (e) {
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY 
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
+canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
+canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY 
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
+}, false);
+
+// Get the position of the mouse relative to the canvas
+function getMousePos(canvasDom, mouseEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+        x: mouseEvent.clientX - rect.left,
+        y: mouseEvent.clientY - rect.top
+    };
+}
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+    };
+}
+
+// Draw to the canvas
+function renderCanvas() {
+    if (drawing) {
+        ctx.moveTo(lastPos.x, lastPos.y);
+        ctx.lineTo(mousePos.x, mousePos.y);
+        ctx.stroke();
+        lastPos = mousePos;
+    }
+}
+
+function clear_canvas() {
+    context.fillStyle = start_background_color;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    restore_array = [];
+    index = -1;
+}
+
+function undo_last() {
+if( index <= 0) {
+    clear_canvas();
+} else {
+    index -= 1;
+    restore_array.pop();
+    context.putImageData(restore_array[index], 0, 0);
+}
+}
